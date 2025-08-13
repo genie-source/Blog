c1fde3f69a6be61c1f04bc74266033e039dd54a3
jinkyung <genie_lee@hufs.ac.kr>
2025-08-13 15:43:29 +0900

    init: router 초기 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,7 +5,10 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/about" element={<h1>about</h1>} />
             </Routes>
         </BrowserRouter>
     )
 }
+
+export default Router
